68e4558a43aab2e5ee989f7731c4942c5791bb63
bastienv <bastien.vanhove76@gmail.com>
2015-03-31 15:22:21 +0200

    Premier commit

---
+++ src/test/java/fr/manastria/ApplicationTest.java
@@ -0,0 +1,15 @@
+package fr.manastria;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+@Test
+public class ApplicationTest
+{
+    @Test
+    public void aaa() {
+        Assert.assertTrue(true);
+    }
+}
+
